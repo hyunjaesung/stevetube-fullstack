ee51c2bd3721c099e52776da0b870e95d9a5261a
stevesung <steveysung91@gmail.com>
2019-03-21 16:38:15 +0900

    Revert "babel error"
    This reverts commit 33799c50e59f98f72acb148648033aaa213e20b2.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 import express from "express";
+=======
+const express = require("express");
+// require 은 module 을 폴더안 어딘가에서 가져오라고함 node_modules폴더가서 express찾을듯
+>>>>>>> parent of 33799c5... babel error
 const app = express();
 
 const PORT = 4001;
@@ -6,6 +11,7 @@ const PORT = 4001;
 const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
 
+<<<<<<< HEAD
 const handleHome = (req, res) => res.send("Hello from my ass");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
@@ -15,3 +21,6 @@ app.get("/", handleHome);
 app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
+=======
+app.listen(PORT, handleListening); // 4000번 포트로 리스닝
+>>>>>>> parent of 33799c5... babel error
