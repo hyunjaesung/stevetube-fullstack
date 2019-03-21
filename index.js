a5cf9a208cfb3b8241dbe10502762caf47103be5
stevesung <steveysung91@gmail.com>
2019-03-21 19:37:17 +0900

    Expressjs :middleware
    middleware

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 
-const PORT = 4002;
+const PORT = 4000;
 
 const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
@@ -10,6 +14,17 @@ const handleListening = () =>
 const handleHome = (req, res) => res.send("Hello from my ass");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
+
+const betweenHome = (req, res, next) => {
+  console.log("Between");
+  next();
+};
+
+app.use(cookieParser());
+app.use(bodyParser.json()); // 옵션있음, 어떤 방식으로 다룰지
+app.use(bodyParser.urelencoded({ extened: true }));
+app.use(morgan("dev")); // 옵션있음
+app.use(helmet());
 
 app.get("/", handleHome);
 
