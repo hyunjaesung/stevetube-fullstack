cf81d9ec5bfe189c0962bf211383f441d896b090
stevesung <steveysung91@gmail.com>
2019-03-25 15:58:20 +0900

    Express.js : html things
    useredit

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -9,9 +9,9 @@ import {
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
-userRouter.get(routes.userDetail, userDetail);
-userRouter.get(routes.editProfile, editProfile);
+//userRouter.get(routes.users, users);
+userRouter.get(routes.editProfile, editProfile); // userDetail이 제일나중에실행되야함 id인식되버리기에
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
