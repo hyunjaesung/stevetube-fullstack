9119d64bddf4bf1bac04574d03d634be9ecd2980
stevesung <steveysung91@gmail.com>
2019-03-26 11:15:42 +0900

    MongoDB : HomeController
    async await try catch

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,16 @@
 import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos: videos });
-}; // 그냥 videos만 적어도됨 변수이름같으면
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    // await 는 async 있어야 쓸수있음, model 안에 여러옵션있음 Find 블라블라
+    res.render("home", { pageTitle: "Home", videos: videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
+};
 
 export const search = (req, res) => {
   const {
