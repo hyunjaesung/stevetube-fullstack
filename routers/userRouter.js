41f91e9e791a497c2a4d4a7fd2b17c2ab8553e4c
stevesung <steveysung91@gmail.com>
2019-04-02 14:25:40 +0900

    Relationships and Route Protection: UserDetail sass

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,14 +2,14 @@ import express from "express";
 import routes from "../routes";
 import {
   userDetail,
-  editProfile,
-  changePassword
+  changePassword,
+  getEditProfile
 } from "../controllers/userController";
 import { onlyPrivate } from "../middleware";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, onlyPrivate, editProfile); // userDetail이 제일나중에실행되야함 id인식되버리기에
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile); // userDetail이 제일나중에실행되야함 id인식되버리기에
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
