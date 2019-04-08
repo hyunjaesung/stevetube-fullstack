6890180e86500da20452d78127f7a5409653b63d
stevesung <steveysung91@gmail.com>
2019-04-08 23:08:01 +0900

    heroku:db change

--- src/db.js
+++ src/db.js
@@ -3,10 +3,14 @@ import mongoose from "mongoose";
 
 dotenv.config(); // 자동으로 .env파일의 자료를 가져와서 process.env.key에 저장해줌
 
-mongoose.connect(process.env.MONGO_URL_PROD, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-}); // datebase 주소를 써준다, 계속 귀찮게 몽구스가 권고하는거 해둠
+mongoose.connect(
+  process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
+  // production 이 true 면 배포환경 트루
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  }
+); // datebase 주소를 써준다, 계속 귀찮게 몽구스가 권고하는거 해둠
 
 const db = mongoose.connection;
 
