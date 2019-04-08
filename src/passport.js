500058ced9cfb51d7c4dff67b6d05920a5eb7224
stevesung <steveysung91@gmail.com>
2019-04-08 23:13:51 +0900

    heroku:github login change

--- src/passport.js
+++ src/passport.js
@@ -28,7 +28,9 @@ passport.use(
     {
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
-      callbackURL: `http://localhost:4000${routes.githubCallback}`
+      callbackURL: `https://glacial-eyrie-29202.herokuapp.com${
+        routes.githubCallback
+      }`
     },
     githubLoginCallback
   )
